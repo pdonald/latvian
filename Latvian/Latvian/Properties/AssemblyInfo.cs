de712058c717a5372084b1f35dfcedfe9942685a
Pēteris Ņikiforovs <p@mak.id.lv>
2014-02-09 15:27:50 +0200

    Initial commit

---
+++ Latvian/Latvian/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Latvian")]
+[assembly: AssemblyDescription("Latvian NLP Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pēteris Ņikiforovs")]
+[assembly: AssemblyProduct("Latvian")]
+[assembly: AssemblyCopyright("Copyright © Pēteris Ņikiforovs 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
