f46a89eec3e4a324dd0f1e8882b8d8a653689476
Pēteris Ņikiforovs <p@mak.id.lv>
2014-02-12 02:16:45 +0200

    License

--- Latvian/Tagging/Perceptron/Helpers/XorShiftRandom.cs
+++ Latvian/Tagging/Perceptron/Helpers/XorShiftRandom.cs
@@ -1,6 +1,21 @@
-﻿namespace Latvian.Tagging.Perceptron
+﻿// Copyright 2014 Pēteris Ņikiforovs
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Latvian.Tagging.Perceptron
 {
     // http://www.jstatsoft.org/v08/i14/paper
+    // http://ayende.com/blog/165185/big-data-search-sorting-randomness
     class XorShiftRandom
     {
         private const uint Y = 842502087;
