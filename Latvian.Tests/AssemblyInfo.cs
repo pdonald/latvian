2b9d8a25aeca9e7eaefc4812a4537b3aac9d62f3
Pēteris Ņikiforovs <p@mak.id.lv>
2014-02-13 01:58:54 +0200

    Build: fix AssemblyInfo, build version, copy license

--- Latvian.Tests/AssemblyInfo.cs
+++ Latvian.Tests/AssemblyInfo.cs
@@ -13,15 +13,14 @@
 // limitations under the License.
 
 using System.Reflection;
+using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Latvian.Tests")]
 [assembly: AssemblyDescription("Latvian NLP Library Tests")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pēteris Ņikiforovs")]
-[assembly: AssemblyProduct("Latvian.Tests")]
+[assembly: AssemblyProduct("Latvian")]
 [assembly: AssemblyCopyright("Copyright © Pēteris Ņikiforovs 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
--- Latvian/AssemblyInfo.cs
+++ Latvian/AssemblyInfo.cs
@@ -14,17 +14,16 @@
 
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Latvian")]
 [assembly: AssemblyDescription("Latvian NLP Library")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Pēteris Ņikiforovs")]
 [assembly: AssemblyProduct("Latvian")]
 [assembly: AssemblyCopyright("Copyright © Pēteris Ņikiforovs 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
 
 [assembly: InternalsVisibleTo("Latvian.Tests")]
